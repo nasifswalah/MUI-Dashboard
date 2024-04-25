ccf2294ea6383fe21e9a670f8bf7308917ea8a9f
Nasif Swalah <nasifswalah@gmail.com>
2024-04-25 23:11:58 +0530

    Added the product table

--- src/app/pages/Products/Products.jsx
+++ src/app/pages/Products/Products.jsx
@@ -1,37 +1,58 @@
 import React from "react";
 import DashboardWraper from "../../components/DashboardWraper";
-import { Typography } from "@mui/material";
+import { DataGrid } from '@mui/x-data-grid';
+import { Paper } from "@mui/material";
 
-const ProductsPage = ()=> {
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'productName', headerName: 'Product Name', width: 130 },
+    { field: 'brandName', headerName: 'Brand Name', width: 130 },
+    {
+      field: 'price',
+      headerName: 'Price',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'quantity',
+      headerName: 'Quantity',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'category',
+      headerName: 'Category',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: true,
+      width: 160,
+      valueGetter: (value, row) => `${row.category || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, brandName: 'Snow', productName: 'Jon', price: 35, quantity: 16, category : 'Mens, Kids' },
+    { id: 2, brandName: 'Lannister', productName: 'Cersei', price: 42, quantity: 16, category: 'Mens, Kids' },
+    { id: 3, brandName: 'Lannister', productName: 'Jaime', price: 45, quantity: 16, category: 'Mens, Kids' },
+    { id: 4, brandName: 'Stark', productName: 'Arya', price: 16, quantity: 16, category: 'Mens, Kids' },
+    { id: 5, brandName: 'Targaryen', productName: 'Daenerys', price: null, quantity: 16, category: 'Mens, Kids' },
+    { id: 6, brandName: 'Melisandre', productName: null, price: 150, quantity: 16, category: 'Mens, Kids' },
+    { id: 7, brandName: 'Clifford', productName: 'Ferrara', price: 44, quantity: 16, category: 'Mens, Kids' },
+    { id: 8, brandName: 'Frances', productName: 'Rossini', price: 36, quantity: 16, category: 'Mens, Kids' },
+    { id: 9, brandName: 'Roxie', productName: 'Harvey', price: 65, quantity: 16, category: 'Mens, Kids' },
+  ];
+  
+const ProductsPage = () => {
     return (
         <DashboardWraper title={"Products"} >
-          <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
-          enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
-          imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
-          Convallis convallis tellus id interdum velit laoreet id donec ultrices.
-          Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
-          nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
-          leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
-          feugiat vivamus at augue. At augue eget arcu dictum varius duis at
-          consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
-          sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
-          eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
-          neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
-          tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
-          sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
-          tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
-          gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
-          et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
-          tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+            <Paper style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    pageSize={5}
+                    rowsPerPageOptions={[5, 10]}
+                    checkboxSelection
+                />
+            </Paper>
         </DashboardWraper>
     );
 };
